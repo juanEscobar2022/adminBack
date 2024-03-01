4a7cb96ccdc4548ea36a0c9ce34d3e14882f9e4f
Camilo <juanescobb91@gmail.com>
2024-03-01 15:19:45 -0500

    seeder admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = new User();
+        $user->name = 'ADMIN2';
+        $user->email = 'usuario@example.com';
+        $user->cedula = '000000';
+        $user->status = '1/3';// se debe cargar con ese valor ya que cuando ingresen por primera ves ese estado los guiara a cambiar la contraseña
+        $user->role = '1'; // se necesita por cuestion de los permisos de rol y el rol 1 es administrativo
+        $user->password = Hash::make('Prueba@123456'); // el angular esta configurado para que solo reciba con al menos un caracter especial 
+        $user->save();
     }
 }
